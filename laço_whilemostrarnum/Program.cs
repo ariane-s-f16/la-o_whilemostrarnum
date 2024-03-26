d0728c06261eaa82f7ed36de46e114a29ee14288
Usuario <Usuario@CPD03-M11>
2024-03-26 10:24:49 -0300

    Adicionar arquivos de projeto.

---
+++ laço_whilemostrarnum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laço_whilemostrarnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+            while (cont >=1 && cont<101) 
+            {
+                Console.WriteLine(cont );
+                cont++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
